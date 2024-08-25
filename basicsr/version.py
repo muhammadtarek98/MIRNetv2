6b2ad81a75976c78e788225c901b1571c6bf2f6d
muahmmad <muahmmadtarek@git>
2024-08-25 20:06:48 +0300

    change in the FUnIE-GAN-PyTorch

--- basicsr/version.py
+++ basicsr/version.py
@@ -1,5 +1,5 @@
 # GENERATED VERSION FILE
-# TIME: Thu Apr 14 13:00:18 2022
-__version__ = '1.2.0+HEAD'
+# TIME: Sun Aug 25 19:38:09 2024
+__version__ = '1.2.0+eb7d395'
 short_version = '1.2.0'
 version_info = (1, 2, 0)
